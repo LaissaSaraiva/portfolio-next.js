1c3328a06d3b6cb8824fc7dbae84563bb7f9815e
Laissa Cunha <laissa.cunha@ed3.com.br>
2024-07-21 22:58:31 -0300

    feat(hero-section): cria componente de botão e finaliza hero-section

--- app/components/pages/home/hero-section/index.tsx
+++ app/components/pages/home/hero-section/index.tsx
@@ -1,5 +1,9 @@
 import { TechBagde } from "@/app/components/tech-bagde";
+import { Button } from "@/app/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
+import { HiArrowNarrowRight } from "react-icons/hi";
+import { TbBrandGithub, TbBrandLinkedin } from "react-icons/tb";
 
 const TECH_BAGDES_ITEMS = [
   {
@@ -22,12 +26,25 @@ const TECH_BAGDES_ITEMS = [
   },
 ];
 
+const MOCK_CONTACTS = [
+  {
+    url: "https://github.com/",
+    icon: <TbBrandGithub />,
+  },
+  {
+    url: "https://github.com/",
+    icon: <TbBrandLinkedin />,
+  },
+];
+
 export const HeroSection = () => {
   return (
-    <section className="w-full h-[755px] bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col justify-end pb-10 sm:pb-32 pt-32 lg:pb-[110px]">
+    <section className="w-full lg:h-[755px] bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col justify-end pb-10 sm:pb-32 pt-32 lg:pb-[110px]">
       <div className="container flex items-start justify-between flex-col-reverse lg:flex-row">
         <div className="w-full lg:max-w-[530px]">
-          <p className="font-mono text-emerald-400">Olá, meu nome é </p>
+          <p className="font-mono text-emerald-400 pt-2 md:pt-0">
+            Olá, meu nome é{" "}
+          </p>
           <h2 className="text-4xl font-medium mt-2">Laíssa Saraiva</h2>
 
           <p className="text-gray-400 my-6 text-sm sm:text-base">
@@ -42,7 +59,24 @@ export const HeroSection = () => {
             ))}
           </div>
 
-          <div>contato</div>
+          <div className="mt-6 lg:mt-10 flex sm:items-center sm:gap-5 flex-col sm:flex-row">
+            <Button className="shadow-button w-max">
+              Entre em contato
+              <HiArrowNarrowRight size={18} />
+            </Button>
+
+            <div className="text-2xl text-gray-600 flex items-center h-20 gap-3">
+              {MOCK_CONTACTS.map((contact, index) => (
+                <Link
+                  href={contact.url}
+                  key={index}
+                  className="hover:text-gray-100 transition-colors"
+                >
+                  {contact.icon}
+                </Link>
+              ))}
+            </div>
+          </div>
         </div>
 
         <Image
@@ -50,7 +84,7 @@ export const HeroSection = () => {
           height={404}
           src="/images/profile-pic.jpeg"
           alt="Foto de Perfil"
-          className="rounded-lg"
+          className="rounded-lg w-[300px] h-[300px] lg:w-[420px] lg:h-[404px] mb-6 lg:mb-0 shadow-2xl object-cover"
         />
       </div>
     </section>
