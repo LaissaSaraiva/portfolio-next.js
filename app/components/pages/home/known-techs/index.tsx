264abc5907d910af85880ac56dfe5f96370b9d17
Laissa Cunha <laissa.cunha@ed3.com.br>
2024-07-22 22:40:54 -0300

    feat: inicia section de Conhecimentos + componente de SectionTitle

---
+++ app/components/pages/home/known-techs/index.tsx
@@ -0,0 +1,11 @@
+import { SectionTitle } from "@/app/components/section-title";
+
+type KnownTechsProps = {};
+
+export const KnownTechs = () => {
+  return (
+    <section className="container py-16">
+      <SectionTitle subtitle="competências" title="Conhecimentos" />
+    </section>
+  );
+};
