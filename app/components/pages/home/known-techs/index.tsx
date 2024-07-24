c4ad8b0bb1125b02f03e7822484634809b3902aa
Laissa Cunha <laissa.cunha@ed3.com.br>
2024-07-23 23:49:27 -0300

    feat: adiciona cards de tecnologias conhecidas

--- app/components/pages/home/known-techs/index.tsx
+++ app/components/pages/home/known-techs/index.tsx
@@ -1,4 +1,6 @@
 import { SectionTitle } from "@/app/components/section-title";
+import { TbBrandNextjs } from "react-icons/tb";
+import { KnowTech } from "./known-tech";
 
 type KnownTechsProps = {};
 
@@ -6,6 +8,19 @@ export const KnownTechs = () => {
   return (
     <section className="container py-16">
       <SectionTitle subtitle="competências" title="Conhecimentos" />
+
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3">
+        {Array.from({ length: 8 }).map((_, index) => (
+          <KnowTech
+            key={index}
+            tech={{
+              icon: <TbBrandNextjs />,
+              name: "Next.js",
+              startDate: "2021-11-01",
+            }}
+          />
+        ))}
+      </div>
     </section>
   );
 };
